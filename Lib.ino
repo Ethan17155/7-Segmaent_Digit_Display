c430e7693e9942a49547f4e97638fa928b12faa8
Ethan17155 <34581001+Ethan17155@users.noreply.github.com>
2018-03-12 17:26:49 -0400

    Update Lib.ino

--- Lib.ino
+++ Lib.ino
@@ -1,6 +1,23 @@
-#ifdef 7_Seg_Disp (String print1) {
+#ifdef 7_Seg_Disp {
+int tl;
+int tr;
+int t;
+int m;
+int bl;
+int br;
+int b;
+void off () {
+    digitalWrite(tl, 0);
+    digitalWrite(tr, 0);
+    digitalWrite(t, 0);
+    digitalWrite(m, 0);
+    digitalWrite(bl, 0);
+    digitalWrite(br, 0);
+    digitalWrite(b, 0);
+}
   if (print1 == "A") {
-    digitalWrite(1, HIGH);
+    off ();
+    digitalWrite(tl, HIGH);
   }
 }
 #endif
